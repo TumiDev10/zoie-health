a8573c9e80865d7bb2dd2e6c211269c3832d9db2
Tumi Mashigo <tumi.mashigo15@gmail.com>
2023-06-28 16:46:55 +0200

    changed the layout

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, NavLink } from "react-router-dom";
 import { Navbar, Nav } from "react-bootstrap";
-import { FaSmile } from "react-icons/fa";
-
 import RandomMeal from "./components/RandomMeal";
 import FavoriteMeals from "./components/FavoriteMeals";
 import MealDetails from "./components/MealDetails";
 import AllRecipes from "./components/AllRecipes";
 import logo from "./img/zoie.png";
-import headerImage from "./img/Header.png";
+import background from './img/background.jpeg'
 import "./App.css";
 
 function App() {
   return (
-    <Router>
+    <Router >
       <Navbar expand="lg" style={{ backgroundColor: 'rgb(243 230 238)'}}>
         <Navbar.Brand href="/">
           <img src={logo} alt="Zoie Health Logo" className="logo-image" style={{ width: '15%', marginLeft: '50px' }} />
@@ -22,20 +20,18 @@ function App() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto"  >
-            <Link to="/" className="nav-link" style={{marginLeft: '-70%'}}>
+            <NavLink exact to="/" className="nav-link" style={{marginLeft: '-70%'}} activeClassName="active-tab">
               Home
-            </Link>
-            <Link to="/favorites" className="nav-link" style={{marginLeft: '30%'}}>
+            </NavLink>
+            <NavLink to="/favorites" className="nav-link" style={{marginLeft: '30%'}} activeClassName="active-tab">
               Favorites
-            </Link>
-            <Link to="/allrecipes" className="nav-link" style={{ backgroundColor: '#A77FB9', borderRadius: '40px', marginLeft: '30%' }}>
+            </NavLink>
+            <NavLink to="/allrecipes" className="nav-link" style={{ marginLeft: '30%' }} activeClassName="active-tab">
               Recipes
-            </Link>
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-
-      
 
       <div className="container mt-4">
         <Switch>
--- src/components/MealCard.js
+++ src/components/MealCard.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
-import { Button, Alert } from "react-bootstrap";
+import { Button, Alert, Modal } from "react-bootstrap";
 import { saveMealLS } from "../utils/localStorage";
+import { FaHeart } from "react-icons/fa";
 import "./MealCard.css";
 
 function MealCard({ meal, showDetailsButton, removeMeal, showLikeButton, smallerCard }) {
-  const [showDetails, setShowDetails] = useState(false);
   const [showMessage, setShowMessage] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [liked, setLiked] = useState(false);
 
   const ingredients = [];
 
@@ -22,54 +24,48 @@ function MealCard({ meal, showDetailsButton, removeMeal, showLikeButton, smaller
   const handleLikeClick = () => {
     saveMealLS(meal.idMeal);
     setShowMessage(true);
+    setLiked(true);
   };
 
-  const toggleDetails = () => {
-    setShowDetails(!showDetails);
+  const toggleModal = () => {
+    setShowModal(!showModal);
+  };
+
+  const handleRemoveClick = () => {
+    removeMeal();
+    toggleModal();
   };
 
   return (
     <div className={`meal-card ${smallerCard ? "meal-card--small" : ""}`}>
       <div className="meal-card__header">
-        <a href={`/meal/${meal.idMeal}`} target="_blank" rel="noopener noreferrer">
-          <img
-            className={`meal-card__image ${smallerCard ? "meal-card__image--small" : ""}`}
-            src={meal.strMealThumb}
-            alt={meal.strMeal}
-          />
-        </a>
-        <div className="meal-card__buttons">
-          {showDetailsButton && (
-            <Button variant="primary" onClick={toggleDetails}>
-              {showDetails ? "Hide Details" : "View Details"}
-            </Button>
-          )}
-          {removeMeal && (
-            <Button variant="danger" onClick={removeMeal} style={{backgroundColor: '#A77FB9', border: 'none'}}>
-              Remove
-            </Button>
-          )}
-          {showLikeButton && (
-            <Button variant="primary" onClick={handleLikeClick} style={{backgroundColor: '#A77FB9', border: 'none'}}>
-              Like
-            </Button>
-          )}
-        </div>
+        <img
+          className={`meal-card__image ${smallerCard ? "meal-card__image--small" : ""}`}
+          src={meal.strMealThumb}
+          alt={meal.strMeal}
+          onClick={toggleModal}
+        />
       </div>
       <div className="meal-card__body">
         <h4 className="meal-card__title">{meal.strMeal}</h4>
-        {showDetails && (
-          <>
-            <h5 className="meal-card__subtitle">Ingredients:</h5>
-            <ul className="meal-card__ingredient-list">
-              {ingredients.map((ingredient, index) => (
-                <li key={index} className="meal-card__ingredient-item">
-                  {ingredient}
-                </li>
-              ))}
-            </ul>
-            <p className="meal-card__instructions">{meal.strInstructions}</p>
-          </>
+        {showDetailsButton && (
+          <Button variant="primary" onClick={toggleModal}>
+            View Details
+          </Button>
+        )}
+        {removeMeal && (
+          <Button variant="danger" onClick={handleRemoveClick} style={{ backgroundColor: '#A77FB9', border: 'none', marginLeft: '36%' }}>
+            X
+          </Button>
+        )}
+        {showLikeButton && (
+          <Button
+            variant="primary"
+            onClick={handleLikeClick}
+            style={{ backgroundColor: '#A77FB9', border: 'none',  }}
+          >
+            <FaHeart color={liked ? "red" : "inherit"} />
+          </Button>
         )}
       </div>
       {showMessage && (
@@ -77,8 +73,30 @@ function MealCard({ meal, showDetailsButton, removeMeal, showLikeButton, smaller
           Meal added to favorites!
         </Alert>
       )}
+      <Modal show={showModal} onHide={toggleModal} centered size="lg">
+        <Modal.Header closeButton>
+          <Modal.Title>{meal.strMeal}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <img
+            className="meal-card__modal-image"
+            src={meal.strMealThumb}
+            alt={meal.strMeal}
+          />
+          <h5 className="meal-card__subtitle">Ingredients:</h5>
+          <ul className="meal-card__ingredient-list">
+            {ingredients.map((ingredient, index) => (
+              <li key={index} className="meal-card__ingredient-item">
+                {ingredient}
+              </li>
+            ))}
+          </ul>
+          <p className="meal-card__instructions">{meal.strInstructions}</p>
+        </Modal.Body>
+      </Modal>
     </div>
   );
 }
 
 export default MealCard;
+
