4b90e6c7d1a2900be79e59b7e777fdc62a46f55d
Tumi Mashigo <tumi.mashigo15@gmail.com>
2023-06-30 15:16:55 +0200

    added a Link button

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { FaUtensils, FaCalendarAlt, FaBookOpen, FaTwitter, FaInstagram, FaLinkedin, FaYoutube } from "react-icons/fa";
 import "./Home.css";
+import { Link } from "react-router-dom/";
+
 
 
 function Home() {
@@ -15,7 +17,7 @@ function Home() {
               className="img-fluid rounded"
               style={{ maxWidth: "80%", maxHeight: "80%" }}
             />
-            <a href="/RandomMeal" className="btn btn-primary mt-3" id="btn">Discover Recipes</a>
+            <Link to={"/RandomMeal"} className="btn btn-primary mt-3" id="btn">Discover Recipes</Link>
           </div>
         </div>
         <div className="col-md-6 d-flex align-items-center">
